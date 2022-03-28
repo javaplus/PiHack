4f3604ad2d7e8c1519833ada70018e4eaaad4072
Barry <btarlton@gmail.com>
2022-03-28 14:58:46 -0400

    suggestions to button.py with functions

--- button.py
+++ button.py
@@ -5,16 +5,24 @@ def setUpBoard():
     GPIO.setwarnings(False)    # Ignore warning for now
     GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
     GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)   # Set pin 8 to be an output pin and set initial value to low (off)
-
     GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)   # Set pin 10 to be an input pin and set initial value to low (off)
 
-def buttonAction():
-    if GPIO.input(10):
-        GPIO.output(8, GPIO.HIGH) # Turn on
-        sleep(1)                  # Sleep for 1 second
-        GPIO.output(8, GPIO.LOW)  # Turn off
-        sleep(1)                  # Sleep for 1 second
+def isButtonDown(button_pin):
+    return GPIO.input(button_pin)
+
+def turnOnLED(pin_number):
+    GPIO.output(pin_number, GPIO.HIGH) # Turn on
+
+def turnOffLED(pin_number):
+    GPIO.output(pin_number, GPIO.LOW)  # Turn off
+    
+def handleButtonDown():
+    turnOnLED(8)
+    sleep(1)                  # Sleep for 1 second
+    turnOffLED(8)
+    sleep(1)                  # Sleep for 1 second
 
 setUpBoard()
 while True: # Run forever
-    buttonAction()
+    if isButtonDown(10):
+        handleButtonDown()
